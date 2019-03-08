49e877f300595925d7f71bb3702feb9b86963f38
squirrelinsouthern <squirrel_000@163.com>
2019-03-08 13:11:40 +0800

    解决留言bug

--- message.js
+++ message.js
@@ -28,7 +28,9 @@ let myform = document.querySelector('.messageform')
 myform.addEventListener('submit', function (e) {
   e.preventDefault()
   let yourname = myform.querySelector('input[name=yourname]').value
+  if(yourname.length===0)return false
   let content = myform.querySelector('input[name=content]').value
+  if(content.length===0)return false
   var leaveMessage = AV.Object.extend('whatmessage');
   var message = new leaveMessage();
   message.save({
@@ -40,7 +42,34 @@ myform.addEventListener('submit', function (e) {
     li.innerText = `${object.attributes.name}:${object.attributes.say}`
     let messagelist = document.querySelector('#messagelist')
     messagelist.appendChild(li)
-    querySelector('input[name=yourname]').value = ''
+    myform.querySelector('input[name=yourname]').value = ''
     myform.querySelector('input[name=content]').value = ''
   })
 });
+
+
+
+
+
+// human 这个对象本身具有属性 name 和 city
+// human.__proto__ 对应的对象（也就是原型）具有物种（species）、走（walk）和使用工具（useTools）这几个属性
+// human.__proto__.constructor === Human 为 true
+
+
+// function Human(name,city){
+// this.name = name;
+// this.city = city;
+// } 
+
+// Human.prototype.species = function(){}
+// Human.prototype.walk = function(){}
+// Human.prototype.useTools = function(){}
+
+// Human.prototype ={
+//   constructor :Human
+// }
+
+
+// var human = new Human({name:'Frank', city: 'Hangzhou'})
+// var human2 = new Human({name:'Jack', city: 'Hangzhou'})
+
